f9df7fa3a96fdd926690c9e6e46110f515814889
Julien De Gouffe <julien.degouffe@ssc-spc.gc.ca>
2025-10-23 16:05:51 -0400

    Depends on is no longer needed.

--- databricks_account_setup.tf
+++ databricks_account_setup.tf
@@ -28,6 +28,4 @@ resource "databricks_mws_permission_assignment" "account-admins-are-workspace-ad
   permissions = ["ADMIN"]
 
   provider = databricks.azure_account
-  
-  depends_on = [ terraform_data.workspace-private-endpoint-resolved-ip ]
 }
