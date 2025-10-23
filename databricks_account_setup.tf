9e5874f97fa5e4abd037b34f1f36a5a82a3473f7
Julien De Gouffe <julien.degouffe@ssc-spc.gc.ca>
2025-10-23 15:38:03 -0400

    GIve it a different name,

--- databricks_account_setup.tf
+++ databricks_account_setup.tf
@@ -35,8 +35,7 @@ resource "databricks_mws_permission_assignment" "account-admins-are-workspace-ad
   depends_on = [ databricks_metastore_assignment.this ]
 }
 
-# current user is admin
-resource "databricks_mws_permission_assignment" "account-admins-are-workspace-admins" {
+resource "databricks_mws_permission_assignment" "current-user-is-workspace-admins" {
 
   workspace_id = azurerm_databricks_workspace.this.workspace_id
   principal_id = databricks_current_user.me.id
